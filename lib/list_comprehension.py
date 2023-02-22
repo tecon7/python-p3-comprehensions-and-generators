7e83fdc46555e14c093642d8a9021c3a62a78051
Teddy Economou <teddyecon@gmail.com>
2023-02-22 12:20:27 -0500

    lab complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_list = []
+    for evens in num_list:
+        if evens % 2 == 0:
+            even_list.append(evens)
 
 def make_exclamation(sentence_list):
-    pass
+    return [phrase+"!" for phrase in sentence_list]
